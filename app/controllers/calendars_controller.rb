e986c9a9e2d2442067a0917f0132775b4fd48a50
Titeinohito <84887486+Titeinohito@users.noreply.github.com>
2021-06-14 11:02:49 +0900

    issue4　再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,11 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+<<<<<<< Updated upstream
+    #binding.pry
+=======
+    binding.pry
+>>>>>>> Stashed changes
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +20,11 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.permit(:date, :plan)
+<<<<<<< Updated upstream
+    params.require(:plan).permit(:date, :plan)
+=======
+    params.require(calendars_url).permit(:date, :plan)
+>>>>>>> Stashed changes
   end
 
   def getWeek
